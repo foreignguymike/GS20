46e635e095495835621e55051718f5df4fc9ed7f
Mike <foreign.guy.mike@gmail.com>
2023-11-04 23:35:14 -0400

    initial art

--- core/src/main/java/com/distraction/gs20/utils/Constants.java
+++ core/src/main/java/com/distraction/gs20/utils/Constants.java
@@ -4,10 +4,10 @@ public class Constants {
 
     public static final String GAME_TITLE = "Gem Snag 20";
 
-    public static final int WIDTH = 128;
-    public static final int HEIGHT = 128;
+    public static final int WIDTH = 270;
+    public static final int HEIGHT = 270;
 
-    public static final int DESKTOP_SCALE = 4;
+    public static final int DESKTOP_SCALE = 2;
     public static final int DESKTOP_WIDTH = WIDTH * DESKTOP_SCALE;
     public static final int DESKTOP_HEIGHT = HEIGHT * DESKTOP_SCALE;
 
